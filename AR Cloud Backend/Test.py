4d20c0ae97c197f1a385cd0b47cd621b3dd361fb
Ankit Sinha <ankitsinha@Ankits-MacBook-Pro.local>
2019-09-28 17:04:55 -0700

    Removinf redundant files

--- AR Cloud Backend/Test.py
+++
@@ -1,5 +0,0 @@
-from geopy.geocoders import Nominatim
-geolocator = Nominatim(user_agent="specify_your_app_name_here")
-location = geolocator.geocode("Buckingham Palace,London")
-print(location.address)
-print((location.latitude, location.longitude))
